ed05e8d815cb1bb93a372317fa512d074b119fbb
sambad-adventure <>
2024-08-06 15:50:55 +0000

    chore: Andrevile deploy develop

--- packages/web-domains/src/home/features/floating-button/components/StartRelayQuestionButton.tsx
+++ packages/web-domains/src/home/features/floating-button/components/StartRelayQuestionButton.tsx
@@ -1,15 +1,16 @@
 import { Button, Txt } from '@sambad/sds/components';
 import { colors, size } from '@sambad/sds/theme';
 import Link from 'next/link';
+import { HTMLAttributes } from 'react';
 
-export const StartRelayQuestionButton = () => {
+export const StartRelayQuestionButton = ({ ...rest }: HTMLAttributes<HTMLButtonElement>) => {
   return (
-    <Link href="#">
-      <Button css={{ height: size['3xl'] }}>
+    <Button css={{ height: size['3xl'] }} {...rest}>
+      <Link href="/start-relay-question">
         <Txt typography="subtitle1" color={colors.white}>
           릴레이 질문 시작하기
         </Txt>
-      </Button>
-    </Link>
+      </Link>
+    </Button>
   );
 };
--- packages/web-domains/src/home/features/notification/services/useNotificationService.ts
+++ packages/web-domains/src/home/features/notification/services/useNotificationService.ts
@@ -1,10 +1,47 @@
-// import { useState } from 'react';
+import { useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 
-// export const useNotificationService = () => {
-//   const [isNotificationExist, setIsNotificationExist] = useState<boolean>(false);
+import { useDialogContext } from '@/common/contexts/DialogProvider';
+import { useGetMeetingInfo } from '@/home/common/apis/queries/useGetMeetingName';
+import { useGetNotification } from '@/home/common/apis/queries/useGetNotification';
+import { isSelectedTargetAtom } from '@/home/common/atoms/home.atom';
 
-//   //   let isSelectedTargetMember = false;
-//   //   let isArrived;
+export const useNotificationService = () => {
+  const setSelectedTarget = useSetAtom(isSelectedTargetAtom);
+  const { isOpen, close, open } = useDialogContext();
+  const { data: meetingInfo } = useGetMeetingInfo({
+    options: { gcTime: Infinity },
+  });
 
-//   return {};
-// };
+  const { data: notfication, isRefetching } = useGetNotification({
+    params: { meetingId: meetingInfo?.meetingIds[0]! },
+    options: {
+      enabled: !!meetingInfo?.meetingIds,
+      refetchInterval: 1000 * 30,
+      select: (data) => {
+        if (data?.contents[0]?.eventType === 'TARGET_MEMBER') {
+          setSelectedTarget(true);
+        }
+
+        return data;
+      },
+    },
+  });
+
+  const handleClose = () => {
+    close();
+  };
+
+  useEffect(() => {
+    if (notfication?.contents[0]) {
+      open();
+    }
+  }, [notfication, isRefetching]);
+
+  return {
+    notfication: notfication?.contents[0],
+    isOpen,
+    handleClose,
+    isRefetching,
+  };
+};
