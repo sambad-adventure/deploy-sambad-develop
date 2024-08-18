af4a391de506cdb95ef6cbde3b3032e88271caed
sambad-adventure <>
2024-08-18 00:30:27 +0000

    chore: Andrevile deploy develop

--- packages/web-domains/src/common/utils/getWebDomain.ts
+++ packages/web-domains/src/common/utils/getWebDomain.ts
@@ -1,6 +1,8 @@
 // const STAGE = process.env.NEXT_PUBLIC_STAGE;
 
 export const getWebDomain = () => {
+  // console.log(process.env.NEXT_PUBLIC_STAGE);
+
   switch (process.env.NEXT_PUBLIC_STAGE) {
     case 'production':
       return 'https://moring.one';
